01ab6d2321719bc4be312dd4d0222d7bf2482699
paulleerentveld <paul@veldnet.com.au>
2022-05-21 16:18:29 +1000

    lab done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor,index) => {
+        return <div key={index}>
+          <h1>{actor.name}</h1>
+          <ul>
+            {actor.movies.map((movie,index) => {
+              return <li key={index}>{movie}</li>
+            })}
+          </ul>
+
+        </div>
+      })}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { movies } from '../data';
 
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+          {movies.map((movie,index) => {
+            return <div key={index}>
+              <h3>{movie.title}</h3>
+              <p>Movie Time:{movie.time}mins</p>
+              <p>MetaScore:{movie.metascore}</p>
+              <p>Genres;</p>
+              <ul>
+                {movie.genres.map((genre,index) => {
+                  return <li key={index}>{genre}</li>
+                })}
+              </ul>
+              </div>
+          })}
     </div>
   );
 };
 
 export default Movies;
+
+
